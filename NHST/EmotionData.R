e0e970bb4c2efe1a95ece50397b6b9f67d1ea167
ncondits3 <ncondits3@gatech.edu>
2024-11-10 09:55:06 -0800

    Minor tweaks to NHST

---
+++ NHST/EmotionData.R
@@ -0,0 +1,35 @@
+# Read and view data
+
+emote <- read.csv('Eerola_emotion/mean_ratings_set1.csv')
+
+colnames(emote) # view column names
+View(emote)
+
+cor(emote[ , 2:9])
+
+# Make plots
+
+with(emote,
+     hist(anger)
+     )
+
+with(emote,
+     plot(tension ~ happy)
+)
+
+with(emote,
+     plot(happy ~ factor(TARGET))
+)
+
+
+# Stats
+
+with(emote,
+     lm(tension ~ energy * anger) |> summary()
+)
+
+with(emote,
+     lm(tension ~ TARGET) |> anova()
+)
+
+
