f59c7081f4562a275d87315dfaafe58f6fa74507
ncondits3 <ncondits3@gatech.edu>
2024-11-10 12:45:07 -0800

    Brought everything together I think.

---
+++ Presentations/Part_2/NHST/QualityData.R
@@ -0,0 +1,20 @@
+# Read and view data
+
+SiSEC <- read.csv('../../Data/SiSEC08_source_separation/SiSEC08_parsed.csv') |> 
+  subset(grepl('Algo', Algorithm))
+
+colnames(SiSEC) # view column names
+View(SiSEC)
+
+# Make plots
+
+with(SiSEC,
+     plot(Ratingscore ~ factor(Algorithm))
+     )
+
+# Stats
+
+with(SiSEC,
+     lm(Ratingscore ~ Algorithm) |> summary()
+     
+     )
